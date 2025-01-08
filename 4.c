1454565e4c86e906b0e3027d6d328c033cd2d5f8
Kiarash Sanei <kiarashsanei5@gmail.com>
2025-01-08 22:40:36 +0330

    fourth attemp but time limit!

---
+++ 4.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int *neighbors;
+    int neighborsCount;
+    int distance;
+} Node;
+
+void freeTree(Node *tree, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        free(tree[i].neighbors);
+    }
+    free(tree);
+}
+
+void setDistance(Node *tree, int root, int parent, int curr_distance)
+{
+    tree[root].distance = curr_distance;
+
+    for (int i = 0; i < tree[root].neighborsCount; i++)
+    {
+        int neighbor = tree[root].neighbors[i];
+        if (neighbor != parent)
+        {
+            setDistance(tree, neighbor, root, curr_distance + 1);
+        }
+    }
+}
+
+int checkCondition(Node *tree, int n)
+{
+    int max_distance = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (tree[i].distance > max_distance)
+        {
+            max_distance = tree[i].distance;
+        }
+    }
+
+    int *level_degrees = calloc(max_distance + 1, sizeof(int));
+    int *level_counts = calloc(max_distance + 1, sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        int dist = tree[i].distance;
+        if (level_counts[dist] == 0)
+        {
+            level_degrees[dist] = tree[i].neighborsCount;
+        }
+        else if (level_degrees[dist] != tree[i].neighborsCount)
+        {
+            free(level_degrees);
+            free(level_counts);
+            return 0;
+        }
+        level_counts[dist]++;
+    }
+
+    free(level_degrees);
+    free(level_counts);
+    return 1;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int *neighborCounts = calloc(n, sizeof(int));
+    int edges[n - 1][2];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        scanf("%d %d", &edges[i][0], &edges[i][1]);
+        edges[i][0]--;
+        edges[i][1]--;
+        neighborCounts[edges[i][0]]++;
+        neighborCounts[edges[i][1]]++;
+    }
+
+    Node *tree = malloc(n * sizeof(Node));
+    for (int i = 0; i < n; i++)
+    {
+        tree[i].neighbors = malloc(neighborCounts[i] * sizeof(int));
+        tree[i].neighborsCount = 0;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        tree[u].neighbors[tree[u].neighborsCount++] = v;
+        tree[v].neighbors[tree[v].neighborsCount++] = u;
+    }
+
+    free(neighborCounts);
+
+    int ans = -1;
+    for (int i = 0; i < n && ans == -1; i++)
+    {
+        setDistance(tree, i, -1, 0);
+        if (checkCondition(tree, n))
+        {
+            ans = i + 1;
+        }
+    }
+
+    printf("%d\n", ans);
+    freeTree(tree, n);
+    return 0;
+}
